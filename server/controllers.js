b93207daf5e8c2ec7ea9658deece8dbcdf653111
Ivan Day <ivanday@utexas.edu>
2023-01-03 12:32:08 -0600

    Add queries used in getReviewMetadata controller

--- server/controllers.js
+++ server/controllers.js
@@ -5,17 +5,41 @@ const credentials = {
   host: "localhost",
   database: "sdc",
   password: "password",
-  port: 3000,
+  port: 5432,
 };
 
 const client = new Client(credentials);
-await client.connect();
+client.connect();
 
 const getReviews = (product_id) => {
-  return client.query(`SELECT * FROM reviews WHERE product_id=${product_id}`)
+  return client.query(`EXPLAIN ANALYZE SELECT * FROM reviews
+  WHERE product_id = ${product_id}
+  `)
 };
 
+const getReviewMetadata = (product_id) => {
+  let result = {};
+  //get characteristic value data for each review
+  client.query(`select cr.characteristic_id, cr.review_id, cr.value from characteristic_reviews cr inner join reviews r on cr.review_id = r.id where product_id = ${product_id};`)
+  .then((response) => {
+    //add the data to our object that will eventually be returned
+    console.log(response.rows);
+    //query for the recommended count on reviews
+    return client.query(`select recommend, rating from reviews where product_id = ${product_id}`)
+  }).then((response) => {
+    console.log(response.rows);
+    //iterate over reviews and tally for returned object
+  })
+}
+
+// getReviews(1).then((response) => {
+//   console.log(response.rows);
+// })
+
+getReviewMetadata(1);
+
 module.exports = {
-  getReviews;
+  getReviews,
+  getReviewMetadata
 }
 
--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 let {getReviews} = require('./controllers.js');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.get('/reviews/:product_id', (req, res) => {
   getReviews(req.query.product_id);
-
 });
 
-app.listen(PORT, (err) => {
+
+app.listen(3000, (err) => {
   if (err) {
     return console.error(err);
   }
-  return console.log(`server is listening on ${PORT}`);
+  return console.log(`server is listening on 3000`);
 });
