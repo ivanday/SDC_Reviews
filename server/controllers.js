b2acbfb186df1474cdfaa6cf2db2554feee7b7d9
Ivan Day <ivanday@utexas.edu>
2023-01-03 14:34:44 -0600

    Implement characteristic adder to result object

--- server/controllers.js
+++ server/controllers.js
@@ -35,20 +35,24 @@ const getReviewMetadata = (product_id) => {
     'characteristics': {}
   };
   //get characteristic value data for each review
-  client.query(`select cr.characteristic_id, cr.review_id, cr.value, "c".name from characteristic_reviews cr inner join reviews r on cr.review_id = r.id inner join "characteristics" "c" on cr.characteristic_id = "c".id where r.product_id = ${product_id};`);
+  client.query(`select cr.characteristic_id, cr.review_id, cr.value, "c".name from characteristic_reviews cr inner join reviews r on cr.review_id = r.id inner join "characteristics" "c" on cr.characteristic_id = "c".id where r.product_id = ${product_id};`)
   .then((response) => {
-    //find characteristic name values
-
+    //set the characteristics names and values
+    response.rows.forEach((row) => {
+      if (result.characteristics[row.name]) {
+        result.characteristics[row.name].push(row.value);
+      } else {
+        result.characteristics[row.name] = [row.value];
+      }
+    })
   })
   .then((response) => {
-    //add the data to our object that will eventually be returned
-    console.log(response.rows);
     //query for the recommended count on reviews
     return client.query(`select recommend, rating from reviews where product_id = ${product_id}`)
   })
   .then((response) => {
-    console.log(response.rows);
     //iterate over reviews and tally for returned object
+
   })
 }
 
