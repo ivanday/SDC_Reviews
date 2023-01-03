39abc4063caff3c0e0f4d25f9b4dacd39d4bcf9f
Ivan Day <ivanday@utexas.edu>
2023-01-03 14:05:44 -0600

    edit query for characteristics to include characteristic names and flesh out result object

--- server/controllers.js
+++ server/controllers.js
@@ -18,15 +18,35 @@ const getReviews = (product_id) => {
 };
 
 const getReviewMetadata = (product_id) => {
-  let result = {};
+  //initialize result object
+  let result = {
+    'product_id': product_id,
+    'ratings': {
+      '1': 0,
+      '2': 0,
+      '3': 0,
+      '4': 0,
+      '5': 0
+    },
+    'recommended': {
+      'false': 0,
+      'true': 0
+    },
+    'characteristics': {}
+  };
   //get characteristic value data for each review
-  client.query(`select cr.characteristic_id, cr.review_id, cr.value from characteristic_reviews cr inner join reviews r on cr.review_id = r.id where product_id = ${product_id};`)
+  client.query(`select cr.characteristic_id, cr.review_id, cr.value, "c".name from characteristic_reviews cr inner join reviews r on cr.review_id = r.id inner join "characteristics" "c" on cr.characteristic_id = "c".id where r.product_id = ${product_id};`);
+  .then((response) => {
+    //find characteristic name values
+
+  })
   .then((response) => {
     //add the data to our object that will eventually be returned
     console.log(response.rows);
     //query for the recommended count on reviews
     return client.query(`select recommend, rating from reviews where product_id = ${product_id}`)
-  }).then((response) => {
+  })
+  .then((response) => {
     console.log(response.rows);
     //iterate over reviews and tally for returned object
   })
